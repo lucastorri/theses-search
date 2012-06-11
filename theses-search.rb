c6a8afec56d0b465068e202d5c39656a959a4d0f
Vinicius Gomes <vvgomess@gmail.com>
2012-06-11 17:47:40 -0300

    more code

--- theses-search.rb
+++ theses-search.rb
@@ -1,18 +1,20 @@
 require 'rubygems'
 require 'bundler'
 require 'open-uri'
-
 Bundler.require :default
 
 service = 'http://localhost:8123/'
 service_search = service + 'search/%s'
 
 get '/' do
+  puts '### INDEX'
   erb :index
 end
 
-get '/:query' do
-  result = URI.parse(service_search % params[:query]).read
+get '/search' do
+  puts '### QUERY'
+  #result = URI.parse(service_search % params[:query]).read
+  result = params[:query]
   erb :results, :locals => {
     :result => result
   }
