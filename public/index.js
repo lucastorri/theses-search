a424c659507c7fbe95e948cd177c1b95f4080b7b
Vinicius Gomes <vvgomess@gmail.com>
2012-06-12 16:28:08 -0300

    adding spinner

--- public/index.js
+++ public/index.js
@@ -10,31 +10,50 @@ $(document).ready(function(){
     var queryText = $('#query').val();
     if(!queryText) 
       return;
-    
-    $.get('/search', { query: queryText }, displayResults);
+
+    $('#results').empty();
+    var spinner = startSpinner($('#results').get(0));
+    $.get('/search', { query: queryText }, function(response) {
+      spinner.stop();
+      $('#template').tmpl(response).appendTo('#results');
+    });
   }
 
-  function displayResults(response) {
-    $('#template').tmpl(response).appendTo('#results');
+  function startSpinner(element) {
+    return new Spinner({
+      lines: 13, // The number of lines to draw
+      length: 8, // The length of each line
+      width: 10, // The line thickness
+      radius: 40, // The radius of the inner circle
+      rotate: 49, // The rotation offset
+      color: '#000', // #rgb or #rrggbb
+      speed: 2, // Rounds per second
+      trail: 100, // Afterglow percentage
+      shadow: true, // Whether to render a shadow
+      hwaccel: true, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    }).spin(element);
   }
+
+  /*var search = function(e) {
+    lastSearch = queryField[0].value.trim();
+    ws.send(lastSearch);
+    location.hash = lastSearch;
+  }
+
+  if(location.hash) {
+    queryField[0].value = location.hash.substring(1);
+    setTimeout(search, 500);
+  }
+
+  queryField.keyup(function() {
+    clearTimeout(searchTimeout);
+    lastSearch !== this.value 
+      && this.value.trim() 
+      && (searchTimeout = setTimeout(search, 500));
+  });*/
 });
 
-/*
-var search = function(e) {
-  lastSearch = queryField[0].value.trim();
-  ws.send(lastSearch);
-  location.hash = lastSearch;
-}
-
-if (location.hash) {
-  queryField[0].value = location.hash.substring(1);
-  setTimeout(search, 500);
-}
-
-queryField.keyup(function() {
-  clearTimeout(searchTimeout);
-  lastSearch !== this.value &&
-        this.value.trim() &&
-        (searchTimeout = setTimeout(search, 500));
-});
-*/
