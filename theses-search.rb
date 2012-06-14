6292bd338c3876dbc50e0dae71057d47410f0995
Lucas Torri <lucastorri@gmail.com>
2012-06-13 21:25:58 -0300

    adding a lens icon to the search box

--- theses-search.rb
+++ theses-search.rb
@@ -3,7 +3,7 @@ require 'bundler'
 require 'open-uri'
 Bundler.require :default
 
-service = 'http://10.27.15.13:8123'
+service = 'http://127.0.0.1:8123'
 
 def encode query
   URI.encode(query.gsub(' ', '+'))
